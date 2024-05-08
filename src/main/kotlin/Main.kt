30d993bc76e55bdfabad2508f420623d7c6d17a7
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-09 00:59:00 +0300

    TG-BOT-lesson-1: Added words.txt the dictionary file. Printing pairs of words to the console

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
 
 fun main() {
-    val wordsFile : File = File("words.txt")
+    val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
+    var reader: BufferedReader? = null
 
-    wordsFile.forEachLine {
-        println(it)
+    reader = BufferedReader(FileReader(wordsFile))
+    var line: String?
+
+    while (reader.readLine().also { line = it } != null) {
+        println(line)
     }
 }
