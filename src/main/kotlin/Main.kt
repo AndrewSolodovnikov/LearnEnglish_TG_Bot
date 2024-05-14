de17a8f4c66ab3559f6267b4663d07db59c8d909
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-14 18:06:57 +0300

    TG-BOT-lesson-1: fixed the code, applied forEach method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,9 @@
-import java.io.BufferedReader
 import java.io.File
-import java.io.FileReader
 
 fun main() {
     val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
-    var reader: BufferedReader? = null
 
-    reader = BufferedReader(FileReader(wordsFile))
-    var line: String?
-
-    while (reader.readLine().also { line = it } != null) {
-        println(line)
-    }
+    val lines = wordsFile.readLines()
+    lines.forEach { println(it) }
 }
