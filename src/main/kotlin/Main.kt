9d80f21110c83af2c4ee6520b4f4f5af61b689f4
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-06-08 22:29:04 +0300

    TG-BOT-lesson-3: add console menu and statistics of learned words

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,7 +13,27 @@ fun main() {
         dictionary.add(Word(line[0], line[1], correctAnswerCount))
     }
 
-    println(dictionary)
+    while (true) {
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+
+        when (readln()) {
+            1.toString() -> TODO("Учим слова (выход из цикла)")
+            2.toString() -> statistics(dictionary)
+            0.toString() -> {
+                println("Приложение закрыто!")
+                break
+            }
+
+            else -> println("Вы ввели неверное значение, попробуйте еще раз!")
+        }
+    }
+}
+
+fun statistics(dictionary: List<Word>) {
+    val countWords = dictionary.size
+    val countLearnedWords = dictionary.filter { it.correctAnswerCount >= 3 }.size
+    val percentageWordsLearned = countLearnedWords * 100 / countWords
+    println("Выучено $countLearnedWords из $countWords | $percentageWordsLearned%")
 }
 
 data class Word(
